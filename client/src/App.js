636f7424c3b1e17631264f3d7b0d1f90c887bce9
sidram1705 <sidram1705@gmail.com>
2023-01-16 12:08:23 +0530

    with dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,22 @@
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
-import List from "./pages/list/List"
-import New from "./pages/new/New"
-import Single from "./pages/single/Single"
+//import New from "./pages/new/New"
+//import Single from "./pages/single/Single"
 import { Route, Routes, BrowserRouter} from "react-router-dom";
+//import Inward from "./components/inward/Inward";
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
+      {<BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home/>} />
             <Route path = "login" element={<Login/>} />
-            <Route path="users">
-              <Route index element={<List/>} />
-              <Route path = ":userId" element={<Single/>} />
-              <Route path = "new" element={<New/>} />
-            </Route>
-            <Route path="products">
-              <Route index element={<List/>} />
-              <Route path = ":ProductId" element={<Single/>} />
-              <Route path = "new" element={<New/>} />
-            </Route>
           </Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> 
+      }
     </div>
   );
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Inward from "../../components/inward/Inward"
+//import Supply from "../../components/supply/Supply"
 import "./home.scss"
 
 const Home = () => {
   return (
     <div className="home">
         <Sidebar/>
-        <div className="homeContainer">container_1</div>
+        <div className="homeContainer">
+          <Navbar/>
+          <Inward/>
+        </div>
     </div>
   )
 }
