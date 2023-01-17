32ecc1b7d284341dca0cd7f6ce6a1f9d2936be80
Jeeva  Pranesh <jpranesh14@gmail.com>
2023-01-17 15:14:52 +0530

    wip dashboard

--- client/src/components/inward/Inward.jsx
+++ client/src/components/inward/Inward.jsx
@@ -15,7 +15,7 @@ const Inward = () => {
         // const token = localStorage.getItem('token');
         const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZmI5ZGY1Y2Q5YWUyNDlkMmI0N2JhZWMwMTljOTNkYzQiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzM5NDUyOTB9.YxvO4knK1Uo1WzNelmpjZSdPwASaJtozo9u_Oguo8F-9oCe0FYdKFnpFCWAoVV9aJo5DoPYovB-40MGgtWcsukxPDouf49m5VKI60gWohq5aAl_IKm_c0edNpvbhebFaGX9M8O3lpIbgqlNZswtzdeOAsu8_QNQmO-sb1PWqOMUgk6tkdEWvcgukeaPXMcxMQprggezB97JCL3eIkJ9f9vLXukQUPIllgyTrtYowUjSipqc0xr1aoQm4BjbD_PcAHDuLKOlKDD-sU70z9IP6bxedR-5njvY6a3FenVi8KSxqp2wD82VoZF7jE0-23XCG_FrczlrC-qTub_Gw5vNfzQ"
         const headers = { Authorization: `Bearer ${token}` };
-        const response = await axios.get('http://127.0.0.1/inward/get?inward_doc_no=CEW/252', { headers });
+        const response = await axios.get('http://lab.zuvatech.com/inward/get?inward_doc_no=CEW/252', { headers });
         setData(response.data);
         console.log(response.data);
       } catch (err) {
