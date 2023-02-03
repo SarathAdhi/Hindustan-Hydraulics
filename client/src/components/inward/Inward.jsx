253bbedb7ad0daaf9c8044bb3905d51db963c875
sidram1705 <sidram1705@gmail.com>
2023-02-03 22:33:07 +0530

    Co-authored-by: Jeeva Pranesh <Jeevapranesh123@users.noreply.github.com>

--- client/src/components/inward/Inward.jsx
+++ client/src/components/inward/Inward.jsx
@@ -1,70 +1,14 @@
 import './inward.scss'
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import API from './api'
+//import React, { useState, useEffect } from 'react';
+//import axios from 'axios';
+//import API from './api'
 
 
 const Inward = () => {
 
-  const [data, setData] = useState({});
+  //const [data, setData] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      
-      try {
-        // const token = localStorage.getItem('token');
-        const api = new API('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZmI5ZGY1Y2Q5YWUyNDlkMmI0N2JhZWMwMTljOTNkYzQiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzM5NDUyOTB9.YxvO4knK1Uo1WzNelmpjZSdPwASaJtozo9u_Oguo8F-9oCe0FYdKFnpFCWAoVV9aJo5DoPYovB-40MGgtWcsukxPDouf49m5VKI60gWohq5aAl_IKm_c0edNpvbhebFaGX9M8O3lpIbgqlNZswtzdeOAsu8_QNQmO-sb1PWqOMUgk6tkdEWvcgukeaPXMcxMQprggezB97JCL3eIkJ9f9vLXukQUPIllgyTrtYowUjSipqc0xr1aoQm4BjbD_PcAHDuLKOlKDD-sU70z9IP6bxedR-5njvY6a3FenVi8KSxqp2wD82VoZF7jE0-23XCG_FrczlrC-qTub_Gw5vNfzQ')
-        const response = api.makeCall('inward/get')
-        console.log("data",response.data)
-
-        const result = [];
-        
-        // eslint-disable-next-line
-        response.data.data.forEach((data,index)=>{{
-          // console.log(data)
-          const results = {};
-          data.store.forEach((store)=>{
-            results[store.store_name] = store.received;
-          })
-          results.inward_reg_no = data.inward_reg_no;
-          results.date = data.date;
-          results.inward_doc_no = data.inward_doc_no;
-          results.supplier_name = data.supplier_name;
-          results.security_inward = data.security_inward;
-          results.materials_received = data.materials_received;
-
-          result.push(
-            <tr>
-              <th scope="row">{results.inward_doc_no}</th>
-              <td>{results.date}</td>
-              <td>{results.supplier_name}</td>
-              <th>{results.smc ? "RECEIVED" : "No"}</th>
-              <th>{results.general ? "RECEIVED" : "No"}</th>
-              <th>{results.instrumentation ? "RECEIVED" : "No"}</th>
-              <th>{results.hydraulics ? "RECEIVED" : "No"}</th>
-              <th>{results.hose ? "RECEIVED" : "No"}</th>
-              <th>{results.tc_counter ? "RECEIVED" : "No"}</th>
-              <th>{results.lc_counter ? "RECEIVED" : "No"}</th>
-              <th>{results.materials_received ? "Yes" : "No"}</th>
-              <th>{results.security_inward ? "Yes" : "No"}</th>
-              <td>{results.inward_reg_no ? results.inward_reg_no : "None"}</td>
-            </tr>
-          )
-        }})
-        console.log("Results",result)
-
-        setData(result);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    const interval = setInterval(() => {
-      fetchData();
-    }, 1000);
-    return () => clearInterval(interval);
     
-  }, []);
-
   return (
     <table class="table">
   <thead>
@@ -85,8 +29,22 @@ const Inward = () => {
     </tr>
   </thead>
   <tbody>
+  <tr>
+      <th scope="row">CEW/252</th>
+      <td>2022-12-29</td>
+      <td>Zuva Inc</td>
+      <td>false</td>
+      <td>false</td>
+      <td>false</td>
+      <td>false</td>
+      <td>false</td>
+      <td>false</td>
+      <td>false</td>
+      <td>false</td>
+      <td>false</td>
+      <td>null</td>
+    </tr>
     
-    {data}
   </tbody>
 </table>
   )
