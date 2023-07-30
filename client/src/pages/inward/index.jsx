514ddaa7d60cf7be4156b64b69be9c850af79bc7
SarathAdhi <s.sarathadhithya@gmail.com>
2023-07-30 17:07:28 +0530

    dashboard changes

--- client/src/pages/inward/index.jsx
+++ client/src/pages/inward/index.jsx
@@ -6,146 +6,228 @@ import { withAuth } from "../../hoc/withAuth";
 import axios from "../../lib/axios";
 import { useEffect, useState } from "react";
 import dayjs from "dayjs";
+import { routingOptions } from "../../utils/constants";
 
 const InwardPage = () => {
-  const [inwardData, setInwardData] = useState([]);
+	const [inwardData, setInwardData] = useState([]);
 
-  async function fetchInwardData() {
-    axios.get("/inward/dashboard").then((res) => setInwardData(res));
-  }
+	async function fetchInwardData() {
+		axios.get("/inward/dashboard").then((res) => setInwardData(res));
+	}
 
-  useEffect(() => {
-    fetchInwardData();
-  }, []);
+	useEffect(() => {
+		fetchInwardData();
+	}, []);
 
-  console.log(inwardData);
+	console.log(inwardData);
 
-  // const columns = [
-  //   {
-  //     accessorKey: "status",
-  //     header: "Status",
-  //   },
-  //   {
-  //     accessorKey: "email",
-  //     header: ({ column }) => {
-  //       return (
-  //         <Button
-  //           variant="ghost"
-  //           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-  //         >
-  //           Email
-  //           <ArrowUpDown className="ml-2 h-4 w-4" />
-  //         </Button>
-  //       );
-  //     },
-  //   },
-  //   {
-  //     accessorKey: "amount",
-  //     header: "Amount",
-  //   },
-  // ];
+	// const columns = [
+	//   {
+	//     accessorKey: "status",
+	//     header: "Status",
+	//   },
+	//   {
+	//     accessorKey: "email",
+	//     header: ({ column }) => {
+	//       return (
+	//         <Button
+	//           variant="ghost"
+	//           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+	//         >
+	//           Email
+	//           <ArrowUpDown className="ml-2 h-4 w-4" />
+	//         </Button>
+	//       );
+	//     },
+	//   },
+	//   {
+	//     accessorKey: "amount",
+	//     header: "Amount",
+	//   },
+	// ];
 
-  const columns = [
-    {
-      accessorKey: "s_no",
-      header: () => <span>S NO</span>,
-    },
-    {
-      accessorKey: "doc_no",
-      header: () => <span>DOC NO</span>,
-    },
-    {
-      accessorKey: "doc_date",
-      header: () => <span>DOC DATE</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("doc_date");
+	const columns = [
+		{
+			accessorKey: "s_no",
+			header: () => <span>S NO</span>,
+		},
+		{
+			accessorKey: "doc_no",
+			header: () => <span>DOC NO</span>,
+		},
+		{
+			accessorKey: "doc_date",
+			header: () => <span>DOC DATE</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("doc_date");
 
-        return dayjs(value).format("DD/MM/YYYY");
-      },
-    },
-    {
-      accessorKey: "supplier_name",
-      header: () => <span>SUPPLIER NAME</span>,
-    },
-    {
-      accessorKey: "smc",
-      header: () => <span>SMC</span>,
-    },
-    {
-      accessorKey: "general",
-      header: () => <span>GENERAL</span>,
-    },
-    {
-      accessorKey: "instrumentation",
-      header: () => <span>INSTRUMENTATION</span>,
-    },
-    {
-      accessorKey: "hydraulics",
-      header: () => <span>HYDRAULICS</span>,
-    },
-    {
-      accessorKey: "hose",
-      header: () => <span>HOSE</span>,
-    },
-    {
-      accessorKey: "tc_counter",
-      header: () => <span>TC COUNTER</span>,
-    },
-    {
-      accessorKey: "lc_counter",
-      header: () => <span>LC COUNTER</span>,
-    },
-    {
-      accessorKey: "materials_received",
-      header: () => <span>MATERIALS RECEIVED</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("materials_received");
+				return dayjs(value).format("DD/MM/YYYY");
+			},
+		},
+		{
+			accessorKey: "supplier_name",
+			header: () => <span>SUPPLIER NAME</span>,
+		},
+		{
+			accessorKey: "smc",
+			header: () => <span>SMC</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const smc = storesValue.find((e) => e.store_name === "smc");
 
-        return value ? "YES" : "NO";
-      },
-    },
-    {
-      accessorKey: "security_inward",
-      header: () => <span>SECURITY INWARD</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("security_inward");
+				return <span>{smc?.received ? "RECEIVED" : ""}</span>;
+			},
+		},
+		{
+			accessorKey: "general",
+			header: () => <span>GENERAL</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const general = storesValue.find(
+					(e) => e.store_name === "general"
+				);
 
-        return value ? "OUT" : "";
-      },
-    },
-    {
-      accessorKey: "inward_reg_no",
-      header: () => <span>REG NO</span>,
-    },
-  ];
+				return <span>{general?.received ? "RECEIVED" : ""}</span>;
+			},
+		},
+		{
+			accessorKey: "instrumentation",
+			header: () => <span>INSTRUMENTATION</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const instrumentation = storesValue.find(
+					(e) => e.store_name === "instrumentation"
+				);
 
-  // const columns = [
-  //   "SL.NO",
-  //   "DOC NO INWARD",
-  //   "Date",
-  //   "SUPPLIER NAME",
-  //   "SMC",
-  //   "GENERAL",
-  //   "INSTRUMENTAL",
-  //   "HYDRAULICS",
-  //   "HOSE",
-  //   "TC COUNTER",
-  //   "LC COUNTER",
-  //   "MATERIALS RECEIVED",
-  //   "SECURITY INWARD",
-  //   "INWARD REG. NO",
-  // ].map((header) => ({
-  //   accessorKey: header.toLowerCase().replace(" ", "_"),
-  //   header: () => (
-  //     <span className="text-center flex flex-col items-center">{header}</span>
-  //   ),
-  // }));
+				return (
+					<span>{instrumentation?.received ? "RECEIVED" : ""}</span>
+				);
+			},
+		},
+		{
+			accessorKey: "hydraulics",
+			header: () => <span>HYDRAULICS</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const hydraulics = storesValue.find(
+					(e) => e.store_name === "hydraulics"
+				);
 
-  return (
-    <PageLayout>
-      <DataTable columns={columns} data={inwardData} />
-    </PageLayout>
-  );
+				return <span>{hydraulics?.received ? "RECEIVED" : ""}</span>;
+			},
+		},
+		{
+			accessorKey: "hose",
+			header: () => <span>HOSE</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const hose = storesValue.find((e) => e.store_name === "hose");
+
+				return <span>{hose?.received ? "RECEIVED" : ""}</span>;
+			},
+		},
+		{
+			accessorKey: "tc_counter",
+			header: () => <span>TC COUNTER</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const tc_counter = storesValue.find(
+					(e) => e.store_name === "tc_counter"
+				);
+
+				return <span>{tc_counter?.received ? "RECEIVED" : ""}</span>;
+			},
+		},
+		{
+			accessorKey: "lc_counter",
+			header: () => <span>LC COUNTER</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const lc_counter = storesValue.find(
+					(e) => e.store_name === "lc_counter"
+				);
+
+				return <span>{lc_counter?.received ? "RECEIVED" : ""}</span>;
+			},
+		},
+		{
+			accessorKey: "routing",
+			header: () => <span>ROUTING</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("routing");
+
+				const type = routingOptions.find((e) => e.value === value);
+
+				return <span>{type?.label}</span>;
+			},
+		},
+		{
+			accessorKey: "routing_name",
+			header: () => <span>ROUTING NAME</span>,
+		},
+		{
+			accessorKey: "routing_receipt_no",
+			header: () => <span>ROUTING RECEIPT NO</span>,
+		},
+		{
+			accessorKey: "materials_received",
+			header: () => <span>MATERIALS RECEIVED</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("materials_received");
+
+				return (
+					<span className={value ? "text-green-600" : "text-red-600"}>
+						{value ? "YES" : "NO"}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "security_inward",
+			header: () => <span>SECURITY INWARD</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("security_inward");
+
+				return (
+					<span className={value ? "text-green-600" : "text-red-600"}>
+						{value ? "YES" : "NO"}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "inward_reg_no",
+			header: () => <span>INWARD REG NO</span>,
+		},
+	];
+
+	// const columns = [
+	//   "SL.NO",
+	//   "DOC NO INWARD",
+	//   "Date",
+	//   "SUPPLIER NAME",
+	//   "SMC",
+	//   "GENERAL",
+	//   "INSTRUMENTAL",
+	//   "HYDRAULICS",
+	//   "HOSE",
+	//   "TC COUNTER",
+	//   "LC COUNTER",
+	//   "MATERIALS RECEIVED",
+	//   "SECURITY INWARD",
+	//   "INWARD REG. NO",
+	// ].map((header) => ({
+	//   accessorKey: header.toLowerCase().replace(" ", "_"),
+	//   header: () => (
+	//     <span className="text-center flex flex-col items-center">{header}</span>
+	//   ),
+	// }));
+
+	return (
+		<PageLayout>
+			<DataTable columns={columns} data={inwardData} />
+		</PageLayout>
+	);
 };
 
 export default withAuth(InwardPage);
--- client/src/pages/supply/index.jsx
+++ client/src/pages/supply/index.jsx
@@ -4,220 +4,312 @@ import { withAuth } from "../../hoc/withAuth";
 import axios from "../../lib/axios";
 import { useEffect, useState } from "react";
 import {
-  docTypeOptions,
-  routingOptions,
-  storeStatusOptions,
+	docTypeOptions,
+	routingOptions,
+	storeStatusOptions,
 } from "../../utils/constants";
 import dayjs from "dayjs";
+import { cn } from "../../lib/utils";
 
 const SupplyPage = () => {
-  const [supplyData, setSupplyData] = useState([]);
+	const [supplyData, setSupplyData] = useState([]);
 
-  const columns = [
-    {
-      accessorKey: "s_no",
-      header: () => <span>S NO</span>,
-    },
-    {
-      accessorKey: "doc_no",
-      header: () => <span>DOC NO</span>,
-    },
-    {
-      accessorKey: "doc_type",
-      header: () => <span>DOC TYPE</span>,
-      cell: ({ row }) => {
-        const rowValue = row.getValue("doc_type");
+	const columns = [
+		{
+			accessorKey: "s_no",
+			header: () => <span>S NO</span>,
+		},
+		{
+			accessorKey: "doc_no",
+			header: () => <span>DOC NO</span>,
+		},
+		{
+			accessorKey: "doc_type",
+			header: () => <span>DOC TYPE</span>,
+			cell: ({ row }) => {
+				const rowValue = row.getValue("doc_type");
 
-        const value = docTypeOptions.find((e) => e.value === rowValue);
+				const value = docTypeOptions.find((e) => e.value === rowValue);
 
-        return <span>{value?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "doc_date",
-      header: () => <span>DOC DATE</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("doc_date");
+				return <span>{value?.label}</span>;
+			},
+		},
+		{
+			accessorKey: "doc_date",
+			header: () => <span>DOC DATE</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("doc_date");
 
-        return dayjs(value).format("DD/MM/YYYY");
-      },
-    },
-    {
-      accessorKey: "po_no",
-      header: () => <span>P O NO</span>,
-    },
-    {
-      accessorKey: "po_date",
-      header: () => <span>P O DATE</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("po_date");
+				return dayjs(value).format("DD/MM/YYYY");
+			},
+		},
+		{
+			accessorKey: "po_no",
+			header: () => <span>P O NO</span>,
+		},
+		{
+			accessorKey: "po_date",
+			header: () => <span>P O DATE</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("po_date");
 
-        return dayjs(value).format("DD/MM/YYYY");
-      },
-    },
-    {
-      accessorKey: "customer_name",
-      header: () => <span>CUSTOMER NAME</span>,
-    },
-    {
-      accessorKey: "smc",
-      header: () => <span>SMC</span>,
-      cell: ({ row }) => {
-        const storesValue = row.original?.store || [];
-        const smc = storesValue.find((e) => e.store_name === "smc");
+				return dayjs(value).format("DD/MM/YYYY");
+			},
+		},
+		{
+			accessorKey: "customer_name",
+			header: () => <span>CUSTOMER NAME</span>,
+		},
+		{
+			accessorKey: "smc",
+			header: () => <span>SMC</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const smc = storesValue.find((e) => e.store_name === "smc");
 
-        const value = storeStatusOptions.find((e) => e.value === smc.supply);
+				const value = storeStatusOptions.find(
+					(e) => e.value === smc?.supply
+				);
 
-        return <span>{value?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "general",
-      header: () => <span>GENERAL</span>,
-      cell: ({ row }) => {
-        const storesValue = row.original?.store || [];
-        const general = storesValue.find((e) => e.store_name === "general");
+				return (
+					<span
+						className={
+							value?.value === "part"
+								? "text-red-600"
+								: "text-green-600"
+						}
+					>
+						{value?.label}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "general",
+			header: () => <span>GENERAL</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const general = storesValue.find(
+					(e) => e.store_name === "general"
+				);
 
-        const value = storeStatusOptions.find(
-          (e) => e.value === general.supply
-        );
+				const value = storeStatusOptions.find(
+					(e) => e.value === general?.supply
+				);
 
-        return <span>{value?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "instrumentation",
-      header: () => <span>INSTRUMENTATION</span>,
-      cell: ({ row }) => {
-        const storesValue = row.original?.store || [];
-        const instrumentation = storesValue.find(
-          (e) => e.store_name === "instrumentation"
-        );
+				return (
+					<span
+						className={
+							value?.value === "part"
+								? "text-red-600"
+								: "text-green-600"
+						}
+					>
+						{value?.label}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "instrumentation",
+			header: () => <span>INSTRUMENTATION</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const instrumentation = storesValue.find(
+					(e) => e.store_name === "instrumentation"
+				);
 
-        const value = storeStatusOptions.find(
-          (e) => e.value === instrumentation.supply
-        );
+				const value = storeStatusOptions.find(
+					(e) => e.value === instrumentation?.supply
+				);
 
-        return <span>{value?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "hydraulics",
-      header: () => <span>HYDRAULICS</span>,
-      cell: ({ row }) => {
-        const storesValue = row.original?.store || [];
-        const hydraulics = storesValue.find(
-          (e) => e.store_name === "hydraulics"
-        );
+				return (
+					<span
+						className={
+							value?.value === "part"
+								? "text-red-600"
+								: "text-green-600"
+						}
+					>
+						{value?.label}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "hydraulics",
+			header: () => <span>HYDRAULICS</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const hydraulics = storesValue.find(
+					(e) => e.store_name === "hydraulics"
+				);
 
-        const value = storeStatusOptions.find(
-          (e) => e.value === hydraulics.supply
-        );
+				const value = storeStatusOptions.find(
+					(e) => e.value === hydraulics?.supply
+				);
 
-        return <span>{value?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "hose",
-      header: () => <span>HOSE</span>,
-      cell: ({ row }) => {
-        const storesValue = row.original?.store || [];
-        const hose = storesValue.find((e) => e.store_name === "hose");
+				return (
+					<span
+						className={
+							value?.value === "part"
+								? "text-red-600"
+								: "text-green-600"
+						}
+					>
+						{value?.label}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "hose",
+			header: () => <span>HOSE</span>,
+			cell: ({ row }) => {
+				const storesValue = row.original?.store || [];
+				const hose = storesValue.find((e) => e.store_name === "hose");
 
-        const value = storeStatusOptions.find((e) => e.value === hose.supply);
+				const value = storeStatusOptions.find(
+					(e) => e.value === hose?.supply
+				);
 
-        return <span>{value?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "counter_no",
-      header: () => <span>COUNTER NO</span>,
-    },
-    {
-      accessorKey: "ready",
-      header: () => <span>READY</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("ready");
+				return (
+					<span
+						className={
+							value?.value === "part"
+								? "text-red-600"
+								: "text-green-600"
+						}
+					>
+						{value?.label}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "counter_no",
+			header: () => <span>COUNTER NO</span>,
+		},
+		{
+			accessorKey: "ready",
+			header: () => <span>READY</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("ready");
 
-        return value ? "YES" : "NO";
-      },
-    },
-    {
-      accessorKey: "ready_to_bill",
-      header: () => <span>READY TO BILL</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("ready_to_bill");
+				return (
+					<span className={cn(value && "text-red-600")}>
+						{value ? "YES" : "NO"}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "ready_to_bill",
+			header: () => <span>READY TO BILL</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("ready_to_bill");
 
-        return value ? "YES" : "NO";
-      },
-    },
-    {
-      accessorKey: "bill_ready",
-      header: () => <span>BILL READY</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("bill_ready");
+				return (
+					<span className={cn(value && "text-[#974806]")}>
+						{value ? "YES" : "NO"}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "bill_ready",
+			header: () => <span>BILL READY</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("bill_ready");
 
-        return value ? "YES" : "NO";
-      },
-    },
-    {
-      accessorKey: "order_status",
-      header: () => <span>ORDER STATUS</span>,
-      cell: ({ row }) => {
-        const rowValue = row.getValue("order_status");
+				return (
+					<span className={cn(value && "text-[#4111f3]")}>
+						{value ? "YES" : "NO"}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "order_status",
+			header: () => <span>ORDER STATUS</span>,
+			cell: ({ row }) => {
+				const rowValue = row.getValue("order_status");
 
-        const value = storeStatusOptions.find((e) => e.value === rowValue);
+				const value = storeStatusOptions.find(
+					(e) => e.value === rowValue
+				);
 
-        return <span>{value?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "bill_no",
-      header: () => <span>BILL NO</span>,
-    },
-    {
-      accessorKey: "routing",
-      header: () => <span>ROUTING</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("routing");
+				return <span>{value?.label}</span>;
+			},
+		},
+		{
+			accessorKey: "bill_no",
+			header: () => <span>BILL NO</span>,
+		},
+		{
+			accessorKey: "routing",
+			header: () => <span>ROUTING</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("routing");
 
-        const type = routingOptions.find((e) => e.value === value);
+				const type = routingOptions.find((e) => e.value === value);
 
-        return <span>{type?.label}</span>;
-      },
-    },
-    {
-      accessorKey: "security_out",
-      header: () => <span>SECURITY OUT</span>,
-      cell: ({ row }) => {
-        const value = row.getValue("security_out");
+				return <span>{type?.label}</span>;
+			},
+		},
+		{
+			accessorKey: "routing_name",
+			header: () => <span>ROUTING NAME</span>,
+		},
+		{
+			accessorKey: "routing_receipt_no",
+			header: () => <span>ROUTING RECEIPT NO</span>,
+		},
+		{
+			accessorKey: "security_out",
+			header: () => <span>SECURITY OUT</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("security_out");
 
-        return value ? "OUT" : "";
-      },
-    },
-    {
-      accessorKey: "reg_no",
-      header: () => <span>REG NO</span>,
-    },
-  ];
+				return (
+					<span className={cn(value && "text-[#006600]")}>
+						{value ? "OUT" : ""}
+					</span>
+				);
+			},
+		},
+		{
+			accessorKey: "reg_no",
+			header: () => <span>REG NO</span>,
+		},
+		{
+			accessorKey: "bill_checked",
+			header: () => <span>BILL CHECKED</span>,
+			cell: ({ row }) => {
+				const value = row.getValue("bill_checked");
 
-  function fetchSupplyData() {
-    axios.get("/supply/dashboard").then((res) => {
-      setSupplyData(res);
-    });
-  }
+				return value ? "YES" : "NO";
+			},
+		},
+	];
 
-  useEffect(() => {
-    fetchSupplyData();
-  }, []);
+	function fetchSupplyData() {
+		axios.get("/supply/dashboard").then((res) => {
+			setSupplyData(res);
+		});
+	}
 
-  console.log({ supplyData });
+	useEffect(() => {
+		fetchSupplyData();
+	}, []);
 
-  return (
-    <PageLayout>
-      <DataTable columns={columns} data={supplyData} />
-    </PageLayout>
-  );
+	console.log({ supplyData });
+
+	return (
+		<PageLayout>
+			<DataTable columns={columns} data={supplyData} />
+		</PageLayout>
+	);
 };
 
 export default withAuth(SupplyPage);
